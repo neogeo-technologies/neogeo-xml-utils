29ca99f080cdee2387d513582952986aa05e0cb4
m431m <m431m@tutanota.com>
2017-03-30 15:59:13 +0200

    Added gitignore, updated setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@ from neogeo_xml_utils import __version__
 
 setup(name='neogeo_xml_utils',
       version=__version__,
-      description='',
-      long_description='',
+      description='Some utils for the ElementTree XML API.',
       author='Neogeo Technologies',
       author_email='contact@neogeo.fr',
       url='https://github.com/neogeo-technologies/neogeo-xml-utils',
-      license='',
+      license='GPLv3',
       classifiers=[],
-      packages=['neogeo_xml_utils'],
-      install_requires=[])
+      packages=['neogeo_xml_utils'])
